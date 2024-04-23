2af814ecedf2065f377d115f80f3148824f7c652
SASA-AKITO <i2486093@cc.kyoto-su.ac.jp>
2024-04-23 11:22:32 +0900

    init cargo

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+/* fn main() {
+    println!("Hello, world!");
+} */
+
+fn hello() -> String {
+    "Hello, world!".to_string()
+}
+fn main() {
+    println!("{}", hello());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_hello() {
+        assert_eq!(hello(), "Hello, world!");
+    }
+}
